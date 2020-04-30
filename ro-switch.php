c01a6264c3287ee8adcb8f0035d4c978432dec0c
Krzysztof Łuczak <krzysztof.luczak.pro@gmail.com>
2020-04-30 09:35:53 +0200

    poprawka, ale jeszcze nie ostatnia

--- ro-switch.php
+++ ro-switch.php
@@ -9,16 +9,16 @@ $functions_file="wp-content/themes/enigma-parallax/functions.php";
 
 $switch = $_GET['s'];
 
-if ($switch == "on") {
+if ($switch == "off") {
     exec("sed -i '/wp-read-only/s/^#//g' $functions_file" );
     exec('find -type f -exec chmod 0444 {} \;');
     exec('find -type d -exec chmod 0555 {} \;');    
-    echo "<h1>Tryb read-only włączony</h1>";
-} elseif ($switch == "off") {
-    exec("sed -i '/wp-read-only/s/^/#/g' $functions_file" );
+    echo "<h1>Możliwość edycji wyłączona</h1>";
+} elseif ($switch == "on") {
     exec('find -type f -exec chmod 0644 {} \;');
     exec('find -type d -exec chmod 0755 {} \;');
-    echo "<h1>Tryb read-only wyłączony</h1>";
+    exec("sed -i '/wp-read-only/s/^/#/g' $functions_file" );
+    echo "<h1>Możliwość edycji włączona</h1>";
 } else {
     echo "<h1>Opcja nieznana</h1>";
 }
